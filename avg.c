b9149589f744a351949ccbb1c2f3d7fc7ed02223
9400093006 <117814828+9400093006@users.noreply.github.com>
2022-11-09 09:18:05 +0000

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,avg;
+printf("enter the value to a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+printf("enter the value to c");
+scanf("%d",&c);
+avg=(a+b+c)/3;
+printf("the avg %d",avg);
+return 0;
+}
---
+++ sample.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,m,add;
+printf("enter the value to n");
+scanf("%d",&n);
+printf("enter value of m");
+scanf("%d",&m);
+add=n+m;
+printf("%d",add);
+
+return 0;
+}
